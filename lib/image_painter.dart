2ba562120e39cf8c0917f15936fb959ea93402f9
RakibulSheam <rakibul.hassan@datumcorp.com>
2025-12-16 08:52:55 +0600

    M4 Image Painter

---
+++ lib/image_painter.dart
@@ -0,0 +1,6 @@
+
+export 'src/_paint_over_image.dart';
+export 'src/controller.dart';
+export 'src/delegates/text_delegate.dart';
+
+
---
+++ test/m4_image_painter_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:m4_image_painter/image_painter.dart';
+
+void main() {
+
+}
